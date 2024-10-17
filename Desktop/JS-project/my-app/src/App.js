73412d327fa6424fe21e921f1b355711eae74407
777DenisEvseev777 <evseevd267@gmail.com>
2024-10-17 15:32:35 +0300

    my first commit

---
+++ Desktop/JS-project/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyPage from './my-site';
+
+function App() {
+  return (
+    <div className="App">
+      <MyPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/JS-project/my-app/src/my-site.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomePage.css';
+
+function MyPage() {
+  return (
+    <div className="homepage">
+      <header className="header">
+        <h1>Ласкаво просимо до мого React додатку!</h1>
+      </header>
+
+      <section className="main-content"></section>
+    </div>
+  );
+}
+
+export default MyPage;
