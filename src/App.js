1d61ee20c5855c9c67dabcd009f14a0c028773e2
DenisEvseevUzhNU <yevsieiev.denys@student.uzhnu.edu.ua>
2024-10-25 22:36:23 +0300

    firts page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import './style/App.css';
-import MyPage from './components/my-site';
-
 
 function App() {
   return (
     <div className="App">
-      <MyPage />
+
+        <div className="loginPage">
+            <div className="loginform">
+
+
+                <div>
+                    <h1 className="text">
+                    Login
+                    </h1>
+                </div>
+                
+
+                  <div className="inputcont">
+                  <h1 className="text3">
+                        Login
+                      </h1>
+                    <input className="Input" type="text" placeholder="login">
+                
+                    </input>
+                  </div>
+                
+                    <div className="inputcont">
+                      <h1  className="text3">
+                        Password
+                      </h1>
+                      <input className="Input"type="password" placeholder="password">
+
+                
+                      </input>
+                  
+                    </div>
+
+                    <div className="buttonform2">
+                      <button className="button">
+                        login
+                      </button>
+                    </div>
+
+                    <div>
+                    <h1 className="text2">
+                      or
+                    </h1>
+                    </div>
+
+                    <div className="buttonform">
+                      <button className="button">
+                        create your account
+                      </button>
+                    </div>
+                
+            </div>
+
+        </div>
+      
     </div>
   );
 }
